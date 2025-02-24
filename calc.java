8856735f32b28cba6e1d92365ae63c33f3e859c4
Einiyae <einiyae6@gmail.com>
2025-02-24 16:23:38 +0530

    mul and div updated from feature2

--- calc.java
+++ calc.java
@@ -1,2 +1,13 @@
-public class calculator{
+public class Calculator {
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
+
